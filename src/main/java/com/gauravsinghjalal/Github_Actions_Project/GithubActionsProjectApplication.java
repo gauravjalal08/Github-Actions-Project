f3312bb5807341c1809f14ace49152a4952668f6
gauravjalal08 <97501904+gauravjalal08@users.noreply.github.com>
2025-06-11 11:21:41 +0500

    Added code by gaurav

---
+++ src/main/java/com/gauravsinghjalal/Github_Actions_Project/GithubActionsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gauravsinghjalal.Github_Actions_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gauravsinghjalal/Github_Actions_Project/GithubActionsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gauravsinghjalal.Github_Actions_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
